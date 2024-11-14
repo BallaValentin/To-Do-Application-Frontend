132ccc2cb1fed939e212b2050738383f74e6cc1f
BValentin22 <ballavalentin18@gmail.com>
2024-11-14 12:58:18 +0200

    Added fetching random advice

---
+++ src/service/AdviceSlipApi.tsx
@@ -0,0 +1,13 @@
+import { Advice } from '../types/Advice';
+
+export async function fetchRandomAdvice(): Promise<Advice> {
+  const response = await fetch('https://api.adviceslip.com/advice');
+  if (!response.ok) {
+    throw new Error('Failed to fetch random advice');
+  }
+  const data = await response.json();
+  return {
+    id: data.slip.id,
+    advice: data.slip.adive,
+  };
+}
