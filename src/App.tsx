2cbc7cf201529a2546740de7bfbf7852c95f6f88
BValentin22 <ballavalentin18@gmail.com>
2024-12-14 17:27:48 +0200

    Added multiple routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 function App() {
-  return <div />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<div>List of todos</div>} />
+        <Route path="/todos/:id" element={<div>Todo with id</div>} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
