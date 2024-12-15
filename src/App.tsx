4a4acc7854c2b15ff30034d31e2d58fa21c9cd1b
BValentin22 <ballavalentin18@gmail.com>
2024-12-15 20:47:24 +0200

    Using ReactQuery to list all ToDos

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,31 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { TodoListPage } from './pages/TodoListPage';
 import { ToDoDetailsPage } from './pages/ToDoDetailsPage';
 import { ToDoUpdatePage } from './pages/ToDoUpdatePage';
 import { ToDoCreatePage } from './pages/ToDoCreatePage';
 
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 10000,
+      refetchInterval: 30000,
+    },
+  },
+});
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<TodoListPage />} />
-        <Route path="/todos/:id" element={<ToDoDetailsPage />} />
-        <Route path="/todos/update/:id" element={<ToDoUpdatePage />} />
-        <Route path="/todos/create" element={<ToDoCreatePage />} />
-      </Routes>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<TodoListPage />} />
+          <Route path="/todos/:id" element={<ToDoDetailsPage />} />
+          <Route path="/todos/update/:id" element={<ToDoUpdatePage />} />
+          <Route path="/todos/create" element={<ToDoCreatePage />} />
+        </Routes>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
--- src/pages/TodoListPage.tsx
+++ src/pages/TodoListPage.tsx
@@ -2,43 +2,46 @@ import React, { useState, useEffect } from 'react';
 import { Alert, AlertColor, Box, Fab } from '@mui/material';
 import { useLocation, useNavigate } from 'react-router-dom';
 import AddIcon from '@mui/icons-material/Add';
-import { ToDo } from '../interface/ToDo';
+import { useQuery } from '@tanstack/react-query';
 import { GetToDos } from '../service/ToDoService';
 import ToDoCard from '../component/card/ToDoCard';
 
 export function TodoListPage() {
   const navigate = useNavigate();
   const location = useLocation();
-  const [todos, setTodos] = useState<ToDo[]>([]);
-  const [error, setError] = useState<string | null>(null);
+
+  const {
+    data: todos,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ['todos'],
+    queryFn: GetToDos,
+  });
+
   const [deleteAlert, setDeleteAlert] = useState<{ severity: AlertColor; message: string } | null>(null);
 
   useEffect(() => {
     if (location.state?.deleteAlert) {
       setDeleteAlert(location.state.deleteAlert);
     }
-    const fetchTodos = async () => {
-      try {
-        const todoData = await GetToDos();
-        setTodos(todoData);
-      } catch (err) {
-        console.error(`Failed to fetch todos: ${err}`);
-        setError('Failed to fetch todos from server. Try again later');
-      }
-    };
-    fetchTodos();
-  }, []);
+  }, [location.state]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div className="error">{error.message}</div>;
+  }
 
   return (
     <Box>
-      {error && <Alert severity="error">{error}</Alert>}
-
       {deleteAlert && <Alert severity={deleteAlert.severity}>{deleteAlert.message}</Alert>}
 
       <h1>List of Todos</h1>
-      {todos.map((todo) => (
-        <ToDoCard key={todo.id} toDo={todo} />
-      ))}
+      {todos && todos.map((todo) => <ToDoCard key={todo.id} toDo={todo} />)}
       <Fab
         color="primary"
         sx={{
