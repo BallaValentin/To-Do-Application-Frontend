a9d77c5505dbe69e8e2ae2ae5f709bfdc6e62f77
BValentin22 <ballavalentin18@gmail.com>
2024-12-15 15:18:12 +0200

    Added ToDoForm and used it in CreateToDoPage

--- src/component/form/ToDoForm.tsx
+++ src/component/form/ToDoForm.tsx
@@ -1,5 +1,143 @@
-function ToDoForm() {
-  return <div>This is a form</div>;
+import { Box, Button, FormControl, FormHelperText, Grid2, TextField, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { ToDo } from '../../interface/ToDo';
+
+interface FormError {
+  title: string;
+  description: string;
+  dueDate: string;
+  levelOfImportance: string;
+}
+
+interface ToDoFormProps {
+  initialValues?: ToDo;
+  onSubmit: (formData: ToDo) => void;
+}
+
+function ToDoForm(toDoForm: ToDoFormProps) {
+  const [formData, setFormData] = useState<ToDo>({
+    title: '',
+    description: '',
+    dueDate: new Date(),
+    levelOfImportance: 0,
+  });
+
+  const [error, setError] = useState<FormError | null>(null);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    let isValid: boolean = true;
+
+    const newErrors: FormError = {
+      title: '',
+      description: '',
+      dueDate: '',
+      levelOfImportance: '',
+    };
+
+    if (!formData?.title) {
+      newErrors.title = 'Title is required';
+      isValid = false;
+    }
+
+    if (!formData?.description) {
+      newErrors.description = 'Description is required';
+      isValid = false;
+    }
+
+    if (!formData?.dueDate) {
+      newErrors.dueDate = 'Due date is required';
+      isValid = false;
+    }
+
+    if (!formData?.levelOfImportance) {
+      newErrors.levelOfImportance = 'Level of Importance is required';
+      isValid = false;
+    }
+
+    if (!isValid) {
+      setError(newErrors);
+    } else {
+      setError(null);
+    }
+
+    toDoForm.onSubmit(formData);
+  };
+
+  return (
+    <Box sx={{ maxWidth: 600, margin: 'auto', padding: 2 }}>
+      <Typography variant="h4" gutterBottom>
+        {toDoForm.initialValues ? 'Update ToDo' : 'Create ToDo'}
+      </Typography>
+      <form onSubmit={handleSubmit}>
+        <Grid2 container spacing={2}>
+          <FormControl fullWidth error={Boolean(error?.title)}>
+            <TextField
+              name="title"
+              label="Title: "
+              value={formData?.title}
+              onChange={handleChange}
+              required
+              variant="outlined"
+              fullWidth
+            />
+            {error?.title && <FormHelperText>{error?.title}</FormHelperText>}
+          </FormControl>
+
+          <FormControl fullWidth error={Boolean(error?.description)}>
+            <TextField
+              name="description"
+              label="Description: "
+              value={formData?.description}
+              onChange={handleChange}
+              required
+              variant="outlined"
+              fullWidth
+            />
+            {error?.description && <FormHelperText>{error?.description}</FormHelperText>}
+          </FormControl>
+
+          <FormControl fullWidth error={Boolean(error?.dueDate)}>
+            <TextField
+              name="dueDate"
+              label="Due date: "
+              type="date"
+              value={formData?.dueDate}
+              onChange={handleChange}
+              required
+              variant="outlined"
+              fullWidth
+            />
+            {error?.dueDate && <FormHelperText>{error?.dueDate}</FormHelperText>}
+          </FormControl>
+
+          <FormControl fullWidth error={Boolean(error?.levelOfImportance)}>
+            <TextField
+              name="levelOfImportance"
+              label="Level of importance: "
+              type="number"
+              value={formData?.levelOfImportance}
+              onChange={handleChange}
+              required
+              variant="outlined"
+              fullWidth
+            />
+            {error?.levelOfImportance && <FormHelperText>{error?.levelOfImportance}</FormHelperText>}
+          </FormControl>
+
+          <Button type="submit" variant="contained" color="primary" fullWidth>
+            {toDoForm.initialValues ? 'Update' : 'Create'} ToDo
+          </Button>
+        </Grid2>
+      </form>
+    </Box>
+  );
 }
 
 export default ToDoForm;
--- src/pages/ToDoCreatePage.tsx
+++ src/pages/ToDoCreatePage.tsx
@@ -1,10 +1,27 @@
-import { Box } from '@mui/material';
+import { Alert, Box } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import ToDoForm from '../component/form/ToDoForm';
+import { CreateToDo } from '../service/ToDoService';
+import { ToDo } from '../interface/ToDo';
 
 export function ToDoCreatePage() {
+  const navigate = useNavigate();
+  const [error, setError] = useState<string | null>(null);
+
+  const handleCreateToDo = async (formData: ToDo) => {
+    try {
+      const todoData = await CreateToDo(formData);
+      navigate(`/todos/${todoData.id}`);
+    } catch (err) {
+      console.log('Failed to create new todo');
+      setError('Failed to create new ToDo. Try again later');
+    }
+  };
   return (
     <Box>
-      <ToDoForm />
+      {error && <Alert severity="error">{error}</Alert>}
+      <ToDoForm onSubmit={handleCreateToDo} />
     </Box>
   );
 }
