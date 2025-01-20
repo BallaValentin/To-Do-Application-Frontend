b62b43381bb066ab4eff8e8c265d8b52c5ba4fda
BValentin22 <ballavalentin18@gmail.com>
2025-01-20 09:20:29 +0200

    Added tooltip for toggle theme button

--- src/component/button/ToggleThemeButton.tsx
+++ src/component/button/ToggleThemeButton.tsx
@@ -1,4 +1,4 @@
-import { Box, IconButton } from '@mui/material';
+import { Box, IconButton, Tooltip, tooltipClasses } from '@mui/material';
 import LightModeIcon from '@mui/icons-material/LightMode';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 import AcUnitIcon from '@mui/icons-material/AcUnit';
@@ -10,10 +10,17 @@ function ToggleThemeButton() {
   const { themeName, setTheme } = useTheme();
 
   const themes = ['light' as Theme, 'dark' as Theme, 'winter' as Theme];
+
   const themeIcons = {
     light: <LightModeIcon />,
     dark: <DarkModeIcon />,
     winter: <AcUnitIcon />,
+  };
+
+  const toggleTexts = {
+    light: 'Toggle dark mode',
+    dark: 'Toggle winter mode',
+    winter: 'Toggle light mode',
   };
 
   const handleToggleTheme = () => {
@@ -26,7 +33,30 @@ function ToggleThemeButton() {
 
   return (
     <Box>
-      <IconButton onClick={handleToggleTheme}>{themeIcons[themeName]}</IconButton>
+      <Tooltip
+        arrow
+        title={toggleTexts[themeName]}
+        slotProps={{
+          popper: {
+            sx: {
+              [`&.${tooltipClasses.popper}[data-popper-placement*="bottom"] .${tooltipClasses.tooltip}`]: {
+                marginTop: '0px',
+              },
+              [`&.${tooltipClasses.popper}[data-popper-placement*="top"] .${tooltipClasses.tooltip}`]: {
+                marginBottom: '0px',
+              },
+              [`&.${tooltipClasses.popper}[data-popper-placement*="right"] .${tooltipClasses.tooltip}`]: {
+                marginLeft: '0px',
+              },
+              [`&.${tooltipClasses.popper}[data-popper-placement*="left"] .${tooltipClasses.tooltip}`]: {
+                marginRight: '0px',
+              },
+            },
+          },
+        }}
+      >
+        <IconButton onClick={handleToggleTheme}>{themeIcons[themeName]}</IconButton>
+      </Tooltip>
     </Box>
   );
 }
